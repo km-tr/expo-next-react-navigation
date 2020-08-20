ede6a7f938766eb6150a39856229373d8ac95745
Yuta Kumagai <y.kumagai@menew.jp>
2020-08-20 22:43:00 +0900

    tmp build

---
+++ build/components/Link/index.d.ts
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { Text } from 'react-native';
+import { NextProps, Web } from './types';
+declare const Link: React.ForwardRefExoticComponent<{
+    children: React.ReactNode;
+    touchableOpacityProps?: import("react-native").TouchableOpacityProps | undefined;
+    style?: import("react-native").TextStyle | undefined;
+    routeName: string;
+    params?: {} | undefined;
+    web?: Web | undefined;
+    native?: {} | undefined;
+    isText?: boolean | undefined;
+} & NextProps & React.RefAttributes<Text>>;
+export default Link;
---
+++ build/components/Link/index.web.d.ts
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Text, View, TextStyle } from 'react-native';
+import { NextProps, Web } from './types';
+/**
+ * Link component for react-navigation and nextjs.
+ *
+ * @param props
+ *  - routeName: string
+ *  - params?: object
+ *  - web?: `{ path?: string; as?: string, replace?: boolean, scroll?: boolean, prefetch?: boolean }`
+ *
+ * ## Usage
+ *
+ * ```diff
+ * -import { TouchableOpacity } from 'react-native'
+ * -...
+ * -<TouchableOpacity onPress={() => navigate({ routeName: 'home' })}>
+ * -  Press me!
+ * - </TouchableOpacity>
+ *
+ * +import { Link } from 'expo-next-react-navigation'
+ * + ...
+ * +<Link routeName="home">
+ * +  Press me!
+ * +</Link>
+ *```
+ *
+ */
+declare const Link: React.ForwardRefExoticComponent<{
+    children: React.ReactNode;
+    touchableOpacityProps?: import("react-native").TouchableOpacityProps | undefined;
+    style?: TextStyle | undefined;
+    routeName: string;
+    params?: {} | undefined;
+    web?: Web | undefined;
+    native?: {} | undefined;
+    isText?: boolean | undefined;
+} & NextProps & React.RefAttributes<Text | View>>;
+export default Link;
